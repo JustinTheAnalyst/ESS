e249e899f416438decbc79dbf35b863c3cc813bd
JustinTheAnalyst <84177062+JustinTheAnalyst@users.noreply.github.com>
2021-08-19 23:30:17 +1200

    remove client info

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -2,7 +2,7 @@
 <div class="page-footer">
 	<div class="row">
 		<div class="col-xs-12 col-sm-6">
-			<span class="txt-color-white">Bearotech <span class="hidden-xs"> - Employee Management System v1.0</span> &copy; 2016-<?php echo date("Y");?></span>
+			<span class="txt-color-white">ABC <span class="hidden-xs"> - ESS Portal</span> &copy; 2016-<?php echo date("Y");?></span>
 		</div>
 	</div>
 </div>
